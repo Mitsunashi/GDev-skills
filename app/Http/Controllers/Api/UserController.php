88375a02e3122558ea33cbd65bbcb5b62bb2b178
mitsunashi <fe14101942>
2021-08-08 23:14:25 -0300

    Initial commit

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json($users);
+    }
+
+    public function signup(Request $request)
+    {
+        return response()->json(['message'=> 'Signup successful']);
+    }
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
